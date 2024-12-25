1a51084775d6ba93baf6b8307186257f29c2b12c
Your Name <mahfuzhasan587@gmail.com>
2024-12-26 02:04:45 +0600

    redux toolkit initialized with axios

--- src/lib/providers/CustomThemeProvider.tsx
+++ src/lib/providers/CustomThemeProvider.tsx
@@ -2,13 +2,17 @@
 import { theme } from "@lib/theme/theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ReactNode } from "react";
+import { Provider } from "react-redux";
+import { store } from "redux/store";
 
 const CustomThemeProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </Provider>
   );
 };
 
